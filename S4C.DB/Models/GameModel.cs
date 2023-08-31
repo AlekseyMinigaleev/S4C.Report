5b885040896fbe9c173b2120ebd803f8f9529f52
Минигалеев Алексей Евгеньевич <minigaleev@nvtech.ru>
2023-08-31 16:48:24 +0500

    #11 DB documentation

--- S4C.DB/Models/GameModel.cs
+++ S4C.DB/Models/GameModel.cs
@@ -1,11 +1,26 @@
 ﻿namespace C4S.DB.Models
 {
+    /// <summary>
+    /// Сущность игры
+    /// </summary>
     public class GameModel
     {
+        /// <summary>
+        /// Id игры
+        /// </summary>
+        /// <remarks>
+        /// Должно быть такое же как и на странице разработчика
+        /// </remarks>
         public int Id { get; private set; }
 
+        /// <summary>
+        /// Название игры
+        /// </summary>
         public string? Name { get; private set; }
 
+        /// <summary>
+        /// Дата публикации
+        /// </summary>
         public DateTime? PublicationDate { get; private set; }
 
         public ISet<GameStatisticModel> GameStatistics { get; private set; }
@@ -18,6 +33,11 @@
             Id = id;
         }
 
+        /// <summary>
+        /// Выполняет обновление сущности
+        /// </summary>
+        /// <param name="name">Название игры</param>
+        /// <param name="publicationDate">дата публикации</param>
         public void Update(string name, DateTime publicationDate)
         {
             Name = name;
