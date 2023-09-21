dcd852bd7508695501754a8a7381f5a7629b8383
Минигалеев Алексей Евгеньевич <minigaleev@nvtech.ru>
2023-09-21 11:09:05 +0500

    #34 Add JToken ExtensionMethod + tests

---
+++ C4S.Helpers/Exceptions/InvalidContractException.cs
@@ -0,0 +1,20 @@
+﻿namespace S4C.Helpers.Exceptions
+{
+    /// <summary>
+    /// Ошибка, указывающая на обновление контракта
+    /// </summary>
+    public class InvalidContractException : Exception
+    {
+        /// <summary>
+        /// Ключ по которому произошла ошибка
+        /// </summary>
+        public string Key { get; private set; }
+
+        /// <param name="key">Ключ по которому произошла ошибка</param>
+        public InvalidContractException(string key) :
+            base($"Ошибка при десериализации ответа. Key = {key}")
+        {
+            Key = key;
+        }
+    }
+}
---
+++ C4S.Helpers/Extensions/JTokenExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json.Linq;
+using S4C.Helpers.Exceptions;
+
+namespace C4S.Helpers.Extensions
+{
+    public static class JTokenExtensions
+    {
+        /// <summary>
+        /// Получает значение типа <typeparamref name="T"/> из указанного <paramref name="jToken"/> по <paramref name="key"/>.
+        /// </summary>
+        /// <typeparam name="T">Тип значения, которое необходимо извлечь.</typeparam>
+        /// <param name="key">Ключ, по которому следует извлечь значение из <see cref="JToken"/>.</param>
+        /// <param name="jToken"><see cref="JToken"/>, из которого нужно извлечь значение.</param>
+        /// <returns>
+        /// Извлеченное значение типа <typeparamref name="T"/>.
+        /// </returns>
+        /// <exception cref="InvalidContractException"/>
+        public static T GetValue<T>(this JToken jToken,
+            string key)
+        {
+            T value;
+            jToken = jToken[key];
+
+            if (jToken is null)
+                throw new InvalidContractException(key);
+
+            if (typeof(T).IsArray)
+                value = jToken.ToObject<T>();
+            else
+                value = jToken.Value<T>();
+
+            return value;
+        }
+
+    }
+}
