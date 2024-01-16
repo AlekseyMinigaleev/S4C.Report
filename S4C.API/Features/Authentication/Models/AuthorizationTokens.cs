c5ae3ed384154698ad4d669f763c3a52b13f9246
Aleksey <113462817+AlekseyMinigaleev@users.noreply.github.com>
2024-01-16 21:37:35 +0500

    #46 (#47)
    Co-authored-by: Минигалеев Алексей Евгеньевич <minigaleev@nvtech.ru>

---
+++ S4C.API/Features/Authentication/Models/AuthorizationTokens.cs
@@ -0,0 +1,18 @@
+﻿namespace С4S.API.Features.Authentication.Models
+{
+    /// <summary>
+    /// бъект для хранения токенов авторизации.
+    /// </summary>
+    public class AuthorizationTokens
+    {
+        /// <summary>
+        /// Токен доступа
+        /// </summary>
+        public string AccessToken { get; set; }
+
+        /// <summary>
+        /// Токен обновления
+        /// </summary>
+        public string RefreshToken { get; set; }
+    }
+}
---
+++ S4C.DB/Migrations/20240115115553_add_RefreshToken_to_UserModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace C4S.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_RefreshToken_to_UserModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiry",
+                table: "User",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiry",
+                table: "User");
+        }
+    }
+}
