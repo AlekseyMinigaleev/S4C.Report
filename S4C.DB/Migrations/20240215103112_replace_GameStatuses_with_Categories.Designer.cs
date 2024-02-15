7ead792821bef5fbe94703cc8ce601a2df7897a9
Минигалеев Алексей Евгеньевич <aleksey.minigaleev@gmail.com>
2024-02-15 15:50:21 +0500

    #68  migration

--- S4C.DB/Migrations/20240215103112_replace_GameStatuses_with_Categories.Designer.cs
+++ S4C.DB/Migrations/20240215103112_replace_GameStatuses_with_Categories.Designer.cs
@@ -4,6 +4,7 @@ using C4S.DB;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace C4S.DB.Migrations
 {
     [DbContext(typeof(ReportDbContext))]
-    partial class ReportDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240215103112_replace_GameStatuses_with_Categories")]
+    partial class replace_GameStatuses_with_Categories
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
