74aa99d16d082c7798a227b6d763b5962d2d42c1
Aleksey <113462817+AlekseyMinigaleev@users.noreply.github.com>
2024-02-16 15:34:37 +0500

    Сервис для синхронизации категорий игр (#71)

---
+++ S4C.API/Features/Category/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using C4S.Helpers.ApiHeplers.Controllers;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using С4S.API.Features.Category.Actions;
+
+namespace С4S.API.Features.Category
+{
+    public class CategoryController : BaseApiController
+    {
+        public CategoryController(IMediator mediator) : base(mediator)
+        { }
+
+        /// <summary>
+        /// Запускает синхронизацию категорий игрк с Яндекса
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        [HttpGet("Sync")]
+        public async Task<ActionResult> SyncCategories(CancellationToken cancellationToken)
+        {
+            var command = new SyncCategories.Command();
+
+            await Mediator.Send(command,cancellationToken);
+
+            return Ok();
+        }
+    }
+}
