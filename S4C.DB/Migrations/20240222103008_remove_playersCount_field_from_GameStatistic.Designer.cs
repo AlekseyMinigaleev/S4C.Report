806aaeda6343c7d3fa40562389bfac3c439aff4f
Минигалеев Алексей Евгеньевич <aleksey.minigaleev@gmail.com>
2024-02-22 15:33:29 +0500

    #97 migration

--- S4C.DB/Migrations/20240222103008_remove_playersCount_field_from_GameStatistic.Designer.cs
+++ S4C.DB/Migrations/20240222103008_remove_playersCount_field_from_GameStatistic.Designer.cs
@@ -4,6 +4,7 @@ using C4S.DB;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace C4S.DB.Migrations
 {
     [DbContext(typeof(ReportDbContext))]
-    partial class ReportDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240222103008_remove_playersCount_field_from_GameStatistic")]
+    partial class remove_playersCount_field_from_GameStatistic
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ S4C.DB/Migrations/20240222103008_remove_playersCount_field_from_GameStatistic.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace C4S.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class remove_playersCount_field_from_GameStatistic : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PlayersCount",
+                table: "GameStatistic");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PlayersCount",
+                table: "GameStatistic",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
